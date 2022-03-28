f4bef005ced07208f6c1e38d85d0a932ed6b8a4a
Antoine Cohen <antoine.cohen@edu.devinci.fr>
2022-03-28 09:09:37 +0200

    Example bluetooth app react

---
+++ findPeripheral.js
@@ -0,0 +1,15 @@
+import get from './node_modules/lodash.get';
+
+export default (peripherals, deviceUUID) => {
+  let found = false;
+  const iterator = peripherals.values();
+
+  for (const item of iterator) {
+    const id = get(item, 'id', null);
+    if (id && id === deviceUUID) {
+      found = item;
+    }
+  }
+
+  return found;
+};
